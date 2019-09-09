0a3b33dc8ffdbea6e7efbef9e7a7e9b9475e6b34
himanshumehta <himanshumehta@10.51.212.113>
2019-09-09 10:27:21 -0400

    initial commit

---
+++ src/SeleniumDemo.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+		
+		
+	}
+
+}
